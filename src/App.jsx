1c2d0b0b697579415468a90cc5d302350d78c6b2
Huzaifa Karim <karimhuzaifa590@gmail.com>
2025-10-01 19:52:49 +0500

    Add FeatureCards section and refactor LogoSection path

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import LogoSection from "./components/LogoSection";
+import LogoSection from "./sections/LogoSection";
 import NavBar from "./components/NavBar";
+import FeatureCards from "./sections/FeatureCards";
 import Hero from "./sections/Hero";
 import ShowcaseSection from "./sections/ShowcaseSection";
 
@@ -10,6 +11,7 @@ const App = () => {
       <Hero />
       <ShowcaseSection />
       <LogoSection />
+      <FeatureCards />
     </>
   );
 };
