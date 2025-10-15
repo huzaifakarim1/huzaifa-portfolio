ab50276e9273e9928c7c6f72ee2c8d88be9dd1bc
Huzaifa Karim <karimhuzaifa590@gmail.com>
2025-10-15 19:39:11 +0500

    Add Footer component and update site metadata

---
+++ src/sections/Footer.jsx
@@ -0,0 +1,29 @@
+import { socialImgs } from "../constants/index";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-container">
+        <div className="flex flex-col justify-center md:items-start items-center">
+          <a href="/">Visit my blog</a>
+        </div>
+        <div className="socials">
+          {socialImgs.map((img) => (
+            <a className="icon" target="_blank" href={img.url} key={img.url}>
+              <img src={img.imgPath} alt="social links" />
+            </a>
+          ))}
+        </div>
+
+        <div className="flex flex-col justify-center">
+          <p className="text-center md:text-end">
+            © {new Date().getFullYear()} Muhammad Huzaifa Karim. All rights
+            reserved.
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
